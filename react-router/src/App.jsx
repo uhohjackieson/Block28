26273c07cd85b9374ce7a2736a439f922917d044
uhohjackieson <jaclynbarbieri@gmail.com>
2023-08-15 15:33:24 -0400

    added yellow and footer

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,19 +1,44 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
+import Yellow from "./components/Yellow";
+
 
 export default function App() {
 
 return (
-<div id="container">
-      <h1>Hello React Router!</h1>
+      <div id="container">
+      <h1 className="header">Hello React Router!</h1>
+
       <div id="navbar">
-      <Routes> 
-        <Route path="/blue" element={<h1>Blue</h1>} />
-      </Routes>
+      
+      <Link to="/home">Home</Link>
+      <Link to="/red">Red</Link>
+      <Link to="/blue">Blue</Link>
+      <Link to="/yellow">Yellow</Link>
+      
       </div>
+
       <div id="main-section">
+
       <Routes> 
-        <Route path="/red" element={<h1>Red</h1>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blue" element={<Blue />} />
+        <Route path="/red" element={<Red />} />
+        <Route path="/yellow" element={<Yellow />} />
       </Routes>
+
       </div>
-    </div>
+
+      <footer className="footer">
+      
+      <Link to="/home">Home</Link>
+      <Link to="/red">Red</Link>
+      <Link to="/blue">Blue</Link>
+      <Link to="/yellow">Yellow</Link>
+      
+      </footer>
+      </div>
+
 )}
